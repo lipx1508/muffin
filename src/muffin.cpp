3782a88a645739644d5116e128fab57b33f87499
lipx <120847863+lipx1508@users.noreply.github.com>
2023-02-11 20:30:52 -0300

    Update muffin.cpp

--- src/muffin.cpp
+++ src/muffin.cpp
@@ -21,6 +21,7 @@ namespace muffin {
         SDL_RWops    * data;
 
         vector<SDL_Texture  *> textures;
+        vector<SDL_Texture  *> buffers;
         vector<SDL_Joystick *> joysticks;
         vector<TTF_Font     *> fonts;
         vector<Mix_Chunk    *> chunks;
@@ -32,7 +33,7 @@ namespace muffin {
 };
 
 // Main
-void muffin::init(const char * title, unsigned int w, unsigned int h, bool fullscreen) {
+void muffin::init(const char * title, unsigned int w, unsigned int h, unsigned int flags) {
     // Basic core handling
     atexit(muffin::close);
 
@@ -42,13 +43,25 @@ void muffin::init(const char * title, unsigned int w, unsigned int h, bool fulls
 
     if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1) throw Mix_GetError();
 
-    runtime::window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_OPENGL | (fullscreen ? SDL_WINDOW_FULLSCREEN : 0));
-    if (runtime::window == NULL) throw SDL_GetError();
+    unsigned int fr = ((flags & MUFFIN_FLAGS_ACCELERATED)        ? SDL_RENDERER_ACCELERATED      : 0) | 
+                      ((flags & MUFFIN_FLAGS_SOFTWARE)           ? SDL_RENDERER_SOFTWARE         : 0) |
+                      ((flags & MUFFIN_FLAGS_VSYNC)              ? SDL_RENDERER_PRESENTVSYNC     : 0), 
+                 fw = ((flags & MUFFIN_FLAGS_FULLSCREEN)         ? SDL_WINDOW_FULLSCREEN         : 0) |
+                      ((flags & MUFFIN_FLAGS_FULLSCREEN_DESKTOP) ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0) | 
+                      ((flags & MUFFIN_FLAGS_BORDERLESS)         ? SDL_WINDOW_BORDERLESS         : 0) | 
+                      ((flags & MUFFIN_FLAGS_RESIZABLE)          ? SDL_WINDOW_RESIZABLE          : 0) | 
+                      ((flags & MUFFIN_FLAGS_MINIMIZED)          ? SDL_WINDOW_MINIMIZED          : 0) | 
+                      ((flags & MUFFIN_FLAGS_MAXIMIZED)          ? SDL_WINDOW_MAXIMIZED          : 0) | 
+                      ((flags & MUFFIN_FLAGS_HIGHDPI)            ? SDL_WINDOW_ALLOW_HIGHDPI      : 0);
 
-    runtime::renderer = SDL_CreateRenderer(runtime::window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
+    runtime::window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, fw);
+    if (runtime::window == NULL) throw SDL_GetError();
+    runtime::renderer = SDL_CreateRenderer(runtime::window, -1, fr);
+    if (runtime::renderer == NULL) throw SDL_GetError();
+
+    // Configures
     SDL_SetRenderDrawBlendMode(runtime::renderer, SDL_BLENDMODE_BLEND);
     SDL_RenderSetLogicalSize(runtime::renderer, w, h);
-    if (runtime::renderer == NULL) throw SDL_GetError();
 }
 
 bool muffin::poll() {
@@ -83,6 +96,10 @@ void muffin::update() {
 
     // Updates renderer
     SDL_RenderPresent(runtime::renderer);
+}
+
+void muffin::delay(unsigned int time) {
+    SDL_Delay(time);
 }
 
 unsigned int muffin::ticksms() {
@@ -158,6 +175,16 @@ int muffin::input::mousey() {
     return y;
 }
 
+void muffin::input::mousecapture(bool capture) {
+    // Sets mouse capture
+    if (SDL_CaptureMouse((SDL_bool)capture) == -1) trace::error(SDL_GetError());
+}
+
+void muffin::input::mousevisibility(bool toggle) {
+    // Sets mouse capture
+    if (SDL_ShowCursor(toggle) == -1) trace::error(SDL_GetError());
+}
+
 // Graphics
 unsigned int muffin::graphics::loadimage(const char * path) {
     // Loads an image and returns it's ID (kinda like OpenGL)
@@ -177,6 +204,14 @@ unsigned int muffin::graphics::loadfont(const char * path, unsigned int size) {
     return runtime::fonts.size() - 1;
 }
 
+unsigned int muffin::graphics::loadcanvas(unsigned int w, unsigned int h) {
+    // Loads a canvas and returns it's ID
+    runtime::buffers.push_back(SDL_CreateTexture(runtime::renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, w, h));
+    if (runtime::buffers.back() == NULL) trace::error(SDL_GetError());
+
+    return runtime::buffers.size();
+}
+
 void muffin::graphics::setcolor(unsigned char r, unsigned char g, unsigned char b, unsigned char a) {
     // Sets current color
     SDL_SetRenderDrawColor(runtime::renderer, r, g, b, a);
@@ -187,8 +222,20 @@ void muffin::graphics::setcolorhex(unsigned long hex) {
     SDL_SetRenderDrawColor(runtime::renderer, hex >> 24, hex >> 16, hex >> 8, hex);
 }
 
+void muffin::graphics::setquality(unsigned int quality) {
+    // Sets current render quality
+    SDL_SetHint("SDL_HINT_RENDER_SCALE_QUALITY", ((quality == MUFFIN_QUALITY_NEAREST)     ? "0" : 
+                                                 ((quality == MUFFIN_QUALITY_LINEAR)      ? "1" : 
+                                                 ((quality == MUFFIN_QUALITY_ANISOTROPIC) ? "2" : "0"))));
+}
+
+void muffin::graphics::setcanvas(unsigned int canvas) {
+    // Sets current render scale
+    SDL_SetRenderTarget(runtime::renderer, (canvas ? runtime::buffers[canvas - 1] : NULL));
+}
+
 void muffin::graphics::setscale(unsigned int size) {
-    // Sets current color
+    // Sets current render scale
     SDL_RenderSetScale(runtime::renderer, size, size);
 }
 
@@ -270,14 +317,12 @@ unsigned int muffin::audio::loadmusic(const char * path) {
 
 void muffin::audio::playaudio(unsigned int id) {
     // Plays audio
-    if (Mix_PlayChannel(-1, runtime::chunks[id], 0) == -1) 
-        trace::error(Mix_GetError());
+    if (Mix_PlayChannel(-1, runtime::chunks[id], 0) == -1) trace::error(Mix_GetError());
 }
 
 void muffin::audio::playmusic(unsigned int id, bool loop) {
     // Plays audio
-    if (Mix_PlayMusic(runtime::musics[id], (loop ? -1 : 0)) == -1) 
-        trace::error(Mix_GetError());
+    if (Mix_PlayMusic(runtime::musics[id], (loop ? -1 : 0)) == -1) trace::error(Mix_GetError());
 }
 
 void muffin::audio::pauseaudio() {
@@ -297,17 +342,38 @@ void muffin::audio::resumemusic() {
 }
 
 void muffin::audio::volumeaudio(unsigned char volume) {
-    Mix_Volume(-1, ((volume > MIX_MAX_VOLUME) ? MIX_MAX_VOLUME : volume));
+    if (Mix_Volume(-1, ((volume > MIX_MAX_VOLUME) ? MIX_MAX_VOLUME : volume)) == -1) trace::error(Mix_GetError());
 }
 
 void muffin::audio::volumemusic(unsigned char volume) {
-    Mix_VolumeMusic(((volume > MIX_MAX_VOLUME) ? MIX_MAX_VOLUME : volume));
+    if (Mix_VolumeMusic(((volume > MIX_MAX_VOLUME) ? MIX_MAX_VOLUME : volume)) == -1) trace::error(Mix_GetError());
 }
 
 // Data
 void muffin::data::loaddata(unsigned char * data, unsigned int size) {
     if (runtime::data != NULL) SDL_RWclose(runtime::data);
     runtime::data = SDL_RWFromMem(data, size);
+    if (runtime::data == NULL) trace::error(SDL_GetError());
+}
+
+// System
+const char * muffin::system::clipboard(const char * data) {
+    const char * prev = SDL_GetClipboardText();
+    if (prev == NULL) trace::error(SDL_GetError());
+    if (SDL_SetClipboardText(data) == -1) trace::error(SDL_GetError());
+    return prev;
+}
+
+const char * muffin::system::prefpath(const char * org, const char * app) {
+    const char * path = SDL_GetPrefPath(org, app);
+    if (path == NULL) trace::error(SDL_GetError());
+    return path;
+}
+
+const char * muffin::system::basepath() {
+    const char * path = SDL_GetBasePath();
+    if (path == NULL) trace::error(SDL_GetError());
+    return path;
 }
 
 // Trace
