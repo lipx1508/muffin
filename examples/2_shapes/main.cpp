45cd5f4c52a941fdd9f2d82235cd74436ac50432
lipx <120847863+lipx1508@users.noreply.github.com>
2023-02-11 20:32:55 -0300

    Update main.cpp

--- examples/2_shapes/main.cpp
+++ examples/2_shapes/main.cpp
@@ -1,13 +1,9 @@
-#ifdef _WIN32
-#include <muffin.hpp>
-#else
 #include <muffin/muffin.hpp>
-#endif
 
 #include <math.h>
 
 int main(int argc, char * argv[]) {
-    muffin::init("muffin | 2 - Shapes", 800, 800);
+    muffin::init("muffin | 2 - Shapes", 800, 800, MUFFIN_FLAGS_VSYNC);
     muffin::graphics::setscale(4);
 
     while (muffin::poll()) {
