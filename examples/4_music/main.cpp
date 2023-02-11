64b5e61bda634a75387dd7f588fc54437b34129a
lipx <120847863+lipx1508@users.noreply.github.com>
2023-02-11 20:34:18 -0300

    Update main.cpp

--- examples/4_music/main.cpp
+++ examples/4_music/main.cpp
@@ -1,11 +1,7 @@
-#ifdef _WIN32
-#include <muffin.hpp>
-#else
 #include <muffin/muffin.hpp>
-#endif
 
 int main(int argc, char * argv[]) {
-    muffin::init("muffin | 4 - Music", 800, 800);
+    muffin::init("muffin | 4 - Music", 800, 800, MUFFIN_FLAGS_VSYNC);
     muffin::graphics::setscale(4);
 
     unsigned int music = muffin::audio::loadmusic("maxwellcat-theme.mp3");
